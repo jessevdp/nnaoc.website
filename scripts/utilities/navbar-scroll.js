bc86d4403bdd0d52d0e1aaf9209bd19f1b0af24c
Jesse van der Pluijm <jesse.vd.pluym@gmail.com>
2019-02-06 21:16:52 +0100

    Create navbar-scroll script and import it

---
+++ scripts/utilities/navbar-scroll.js
@@ -0,0 +1,25 @@
+; (function () {
+    var boundry = 20;
+    var selector = '.navbar-top.navbar-bg-transparent';
+    $elements = document.querySelectorAll(selector);
+
+    for (var i = 0; i < $elements.length; i++) {
+        const $el = $elements[i];
+        addScrollListenerFor($el);
+    }
+
+    function addScrollListenerFor($el) {
+        document.addEventListener('scroll', function (e) {
+            if (window.scrollY > boundry) setScrolled($el);
+            else removeScrolled($el);
+        })
+    }
+
+    function setScrolled($el) {
+        $el.dataset.scrolled = true;
+    }
+
+    function removeScrolled($el) {
+        $el.dataset.scrolled = false;
+    }
+})();
